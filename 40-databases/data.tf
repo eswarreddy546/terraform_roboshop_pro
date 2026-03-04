8e12b6b90c3507c6a5a4ec6f9f46c963a24a34e5
Asri <asri>
2026-03-04 15:45:44 +0530

    terraform modules

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,22 +1,22 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_security_group_id"
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "mongodb" {
-  ami                    = data.aws_ami.amazon_linux.id
+  ami                    = "ami-0220d79f3f480ecf5"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.mongodb_sg_id]
   subnet_id              = local.database_subnetsg
 }
 
 resource "terraform_data" "mongodb" {
+
   triggers_replace = [
     aws_instance.mongodb.id
   ]
-  
+
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -17,11 +18,10 @@ resource "terraform_data" "mongodb" {
     host     = aws_instance.mongodb.private_ip
   }
 
-
-
   provisioner "remote-exec" {
     inline = [
-         "echo Hello world"
+      "echo Hello world"
     ]
   }
+
 }
