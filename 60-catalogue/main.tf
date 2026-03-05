60e2cbbb666deaa930b556157a7215b40612dd7a
Asri <asri>
2026-03-05 21:25:45 +0530

    terraform modules

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -3,13 +3,19 @@ resource "aws_instance" "catalogue" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.catalogue_security_group_id]
   subnet_id              = local.private_subnetsg
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-catalogue"
+  }
 }
 
+
 resource "terraform_data" "catalogue" {
+
   triggers_replace = [
     aws_instance.catalogue.id
   ]
-  
+
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -17,17 +23,17 @@ resource "terraform_data" "catalogue" {
     host     = aws_instance.catalogue.private_ip
   }
 
-  # terraform copies this file to mongodb server
+  # copy script to server
   provisioner "file" {
-    source = "catalogue.sh"
+    source      = "catalogue.sh"
     destination = "/tmp/catalogue.sh"
   }
 
+  # execute script
   provisioner "remote-exec" {
     inline = [
-        "chmod +x /tmp/catalogue.sh",
-        # "sudo sh /tmp/catalogue.sh"
-        "sudo sh /tmp/catalogue.sh catalogue"
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
     ]
   }
 }
